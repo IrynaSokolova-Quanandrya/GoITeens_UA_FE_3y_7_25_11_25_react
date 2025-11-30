4d4baa1bbf4491a4e1939bf07ef6cfd7aef20d6f
Iryna <irasokolova25@ukr.net>
2025-11-30 15:00:18 +0200

    add template

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+const App = () => {
+	return (
+		<div className='main'>
+			<h1>Компоненти</h1>
+			<h2>Сьогодні ми вивчимо:</h2>
+			<ol>
+				<li>
+					<p>Що таке функціональні компоненти</p>
+				</li>
+				<li>
+					<p>Властивості компонента (props)</p>
+				</li>
+				<li>
+					<p>Значення пропсів за замовчуванням</p>
+				</li>
+				<li>
+					<p>Властивість props.children</p>
+				</li>
+				<li>
+					<p>Властивість propTypes</p>
+				</li>
+			</ol>
+		</div>
+	)
+}
+
+export default App
