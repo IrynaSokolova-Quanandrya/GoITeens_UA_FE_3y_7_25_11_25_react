bf624febf763096d313d96335738a06eef841bde
Iryna <irasokolova25@ukr.net>
2025-12-10 16:17:48 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,30 @@ import paintingData from './painting.json'
 const App = () => {
 	return (
 		<div className='main'>
-            {/* <LessonTheme /> */}
-            <PaintingList data={ paintingData} />
+            <List data={ paintingData} />
 		</div>
 	)
 }
-// function A(a, b) {
-	
-// 	return a+b
-// }
-// A(10, 15)
-// A(20, 47)
 
-// function b() {
-// 	console.log('Hello');
-// }
-// b()
-export default App
+const List = ({data}) => {
+	return (
+        <ul >
+            {
+                data.map((rec) => {
+                    return <Item title={rec.title} price={ rec.price} />
+                })
+            }
+          
+		</ul>
+	)
+}
 
-
-
-/**
- * 
-    <li>
-    <img src="" alt="..." width="480"/>
-    <h2></h2>
-    <p>
-        Автор: <a href=""></a>
-    </p>
-    <p>Ціна: грн</p>
-    <p>Наявність:  </p>
-    <button type="button">Додати в кошик</button>
-</li>
- */
+const Item = ({title, price}) => {
+	return (
+		<li >
+            <h2>tytle item</h2>
+            <p>{ title}</p>
+            <p>{ price}</p>
+		</li>
+	)
+}
